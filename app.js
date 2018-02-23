29688c64ced271b818e6981c017c78ff0f212269
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-02-23 20:16:00 +0100

    Add python script

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Require
 const express = require('express');
 const compression = require('compression');
+const PythonShell = require('python-shell');
 
 // Create express server
 const app = express();
 
+const pyScript = new PythonShell('./python/run.py');
 
 // Express middleware
 app.use(compression());
@@ -15,3 +17,19 @@ app.listen(3400, () =>
   console.log('Webserver listening on port 3400')
 );
 
+// sends a message to the Python script via stdin
+pyScript.send('hello');
+ 
+pyScript.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement)
+  console.log(message);
+});
+
+// end the input stream and allow the process to exit
+pyScript.end(function (err,code,signal) {
+  if (err) throw err;
+  console.log('The exit code was: ' + code);
+  console.log('The exit signal was: ' + signal);
+  console.log('finished');
+  console.log('finished');
+});
