895f121851ac2430de9e35b41d93c96244d720d3
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-02-25 17:08:19 +0100

    Add image handling in Node script

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
-const express = require('express');
-const compression = require('compression');
-const PythonShell = require('python-shell');
+const express = require('express')
+const compression = require('compression')
+const PythonShell = require('python-shell')
+const fs = require('fs')
 
 // Create express server
-const app = express();
+const app = express()
 
 // @ts-ignore
-const pyScript = new PythonShell('./python/run.py');
+const pyScript = new PythonShell('./python/run.py')
+// @ts-ignore
+const pyImage = new PythonShell('./python/image-mediator.py', {
+  mode: 'binary'
+})
 
 // Express middleware
-app.use(compression());
+app.use(compression())
 // app.use(express.static('public'));
 
 // Start server
-app.listen(3400, () =>
-  console.log('Webserver listening on port 3400')
-);
+app.listen(3400, () => console.log('Webserver listening on port 3400'))
 
-// sends a message to the Python script via stdin
+// Send to stdin
 console.log('Sending \x1b[34m%s\x1b[0m Python: I am looking for snakes', 'TO')
-pyScript.send('I am looking for snakes');
-// pyScript.send('hello');
- 
-pyScript.on('message', function (message) {
-  // received a message sent from the Python script (a simple "print" statement)
-  console.log('Received \x1b[31m%s\x1b[0m Python: ' + message, 'FROM');
-});
+pyScript.send('I am looking for snakes')
 
-// end the input stream and allow the process to exit
-pyScript.end(function (err,code,signal) {
-  if (err) throw err;
-  console.log('The exit code was: ' + code);
-  console.log('The exit signal was: ' + signal);
-  console.log('finished');
-});
+pyScript.on('message', function (message) {
+  console.log('Received \x1b[31m%s\x1b[0m Python: ' + message, 'FROM')
+})
+
+
+pyImage.on('message' function(message) {
+  console.log('Received \x1b[31m%s\x1b[0m Python: ' + message, 'FROM')
+})
+
+
+fs.readFile('medusa.jpg', function(err, data) {
+  pyImage.send(data)
+})
+
+// Handle end of input streams
+pyScript.end(function (err, code, signal) {
+  if (err) throw err
+  console.log('The exit code was: ' + code)
+  console.log('The exit signal was: ' + signal)
+  console.log('finished')
+})
+
+pyScript.end(function (err, code, signal) {
+  if (err) throw err
+  console.log('The exit code was: ' + code)
+  console.log('The exit signal was: ' + signal)
+  console.log('finished')
+})
