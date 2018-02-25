44705c5e4be2604b7ed37ef4fed66611ea7f7e52
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-02-26 00:59:36 +0100

    Format

--- python/run.py
+++ python/run.py
@@ -1,11 +1,13 @@
 import sys
 
+
 def main():
     input_data = sys.stdin.readline()
     if input_data:
         sys.stdout.write('Python received: ' + input_data)
         print('You have found snakes')
         sys.stdout.flush()
+
 
 if __name__ == '__main__':
     main()
