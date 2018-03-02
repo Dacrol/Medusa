33c8fd130d18b48d161050f7689f352dfd2454f3
Dacrol <jonas.ahrens@gmail.com>
2018-03-02 19:53:36 +0100

    Succesfully load attributes table

--- python/training/train.py
+++ python/training/train.py
@@ -1,4 +1,8 @@
 from filterdataset.main import Filterer
 
-filterer = Filterer()
-filterer.getfilenames()
+# filterer = Filterer()
+filterer = Filterer(attributes_file='python/training/dataset/attributes.txt')
+filenames = filterer.get_filenames()
+
+print(filenames)
+print(filenames.shape)
