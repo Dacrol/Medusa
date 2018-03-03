a0ab6091962f1f6ecefbd298fed23e3cdb34debc
Dacrol <jonas.ahrens@gmail.com>
2018-03-03 23:47:12 +0100

    Set attribute to 8 ('Black_Hair')

--- python/training/train.py
+++ python/training/train.py
@@ -2,7 +2,7 @@ from filterdataset.main import Filterer
 
 # filterer = Filterer()
 filterer = Filterer(attributes_file='python/training/dataset/attributes.txt')
-filenames = filterer.get_filenames()
+filenames = filterer.get_filenames(attributes=(8,))
 
 print(filenames)
 print(filenames.shape)
