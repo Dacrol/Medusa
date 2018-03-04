39bde0e8c410c4ec8fc9d5af57cf2f1fa7ceaed0
Dacrol <jonas.ahrens@gmail.com>
2018-03-04 18:59:19 +0100

    Change queried attributes to strings

--- python/training/train.py
+++ python/training/train.py
@@ -2,7 +2,10 @@ from filterdataset.main import Filterer
 
 # filterer = Filterer()
 filterer = Filterer(attributes_file='python/training/dataset/attributes.txt')
-filenames = filterer.get_filenames(attributes=(8,))
+filenames = filterer.get_filenames(attributes=('Black_Hair', '5_o_Clock_Shadow'))
 
-print(filenames)
+print(filenames[0:10])
+print(filenames[0:-10])
+print(filenames['label'])
+print(filenames['filename'])
 print(filenames.shape)
