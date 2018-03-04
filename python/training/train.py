12e052fde9ce717eac7163c717147acfce9cc3d4
Dacrol <jonas.ahrens@gmail.com>
2018-03-05 00:41:16 +0100

    Add negated test attributes

--- python/training/train.py
+++ python/training/train.py
@@ -2,7 +2,7 @@ from filterdataset.main import Filterer
 
 # filterer = Filterer()
 filterer = Filterer(attributes_file='python/training/dataset/attributes.txt')
-filenames = filterer.get_filenames(attributes=('Black_Hair', '5_o_Clock_Shadow'))
+filenames = filterer.get_filenames(attributes=('Arched_Eyebrows', '5_o_Clock_Shadow', 'Black_Hair'), notattributes='Male')
 
 print(filenames[0:10])
 print(filenames[0:-10])
