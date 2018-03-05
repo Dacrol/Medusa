86bbfc6789a74ac9c9f02ac9849cd2ab6bdd7436
Dacrol <jonas.ahrens@gmail.com>
2018-03-05 02:46:24 +0100

    Print matches and mismatches for comparison

--- python/training/train.py
+++ python/training/train.py
@@ -2,10 +2,8 @@ from filterdataset.main import Filterer
 
 # filterer = Filterer()
 filterer = Filterer(attributes_file='python/training/dataset/attributes.txt')
-filenames = filterer.get_filenames(attributes=('Arched_Eyebrows', '5_o_Clock_Shadow', 'Black_Hair'), notattributes='Male')
+results = filterer.get_filenames(attributes=(
+    'Arched_Eyebrows', 'Black_Hair'), notattributes='Male')
 
-print(filenames[0:10])
-print(filenames[0:-10])
-print(filenames['label'])
-print(filenames['filename'])
-print(filenames.shape)
+print('Matches: ' + str((results['label'] == 1).sum()))
+print('Mismatches: ' + str((results['label'] == -1).sum()))
